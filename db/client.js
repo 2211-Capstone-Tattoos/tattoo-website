604f6d92afea973d2b9c493814aa17e6a32301d7
dkwhiting <dallinkwhiting@gmail.com>
2023-01-19 09:50:51 -0700

    Changed local DB url

--- db/client.js
+++ db/client.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/exampleDB';
+const connectionString = process.env.DATABASE_URL || 'https://localhost:5432/exampledb';
 
 const client = new Pool({
   connectionString,
