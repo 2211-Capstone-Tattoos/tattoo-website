11c5975640dc7f93dcc946d5b25f279c4ac412cf
dizzie <dizziefalls@gmail.com>
2023-01-20 10:07:28 -0600

    more shopAPI

--- client/src/api/shopAPI.js
+++ client/src/api/shopAPI.js
@@ -8,12 +8,26 @@ export const shopAPI = createApi({
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => `products`,
+      
+      //tags allow for 'stateless' rendering based on the application logic only using RTK query. The number of API calls will increase, but due to caching, the average packet size should decrease. It also greatly simplifies our app by allowing the UI actions to dictate what is fetched internally by RTK query. The logic is held internally by the query system, removing the need for reducer actions called inside useEffects on each pertinent component. Fantastic.
 
       //re-fetch *all* products upon tag invalidation
       //we can also have individual tag subscriptions with id: id
-      providesTags: [{type: 'Products', id: 'LIST'}]
+      providesTags: (result) => 
+      result
+      ? [
+          //re-fetch if subscribed to a product containing 'id' upon invalidation
+          //this will return an array of tags with each product's id and the 'LIST' id invalidated on addProduct
+          ...result.map(({id}) => ({ type: 'Products', id })),
+          { type: 'Products', id: 'LIST' }
+        ]
+      : //else upon error, still re-fetch all products
+        [{ type: 'Products', id: 'LIST' }]
     }),
-    //getProduct:
+    getProduct: builder.query({
+      query: (id) => `products/${id}`,
+      providesTags: (result, error, id) => [{ type: 'Products', id }]
+    }),
     addProduct: builder.mutation({
       query(body) {
         return {
