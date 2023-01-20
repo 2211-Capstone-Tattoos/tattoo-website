bccfa9608f3401b1f6762f7e873113d07b328850
Anthony <anthonypenaloza99@gmail.com>
2023-01-20 12:25:18 -0600

    Login endpoint and bcrypt/jsonwebtoken are done

--- api/users.js
+++ api/users.js
@@ -1,11 +1,39 @@
+const { getUserByUsername } = require('../db');
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const router = require('express').Router();
 
-// GET: api/users
-router.get('/', async (req, res, next) => {
+// POST /api/users/login
+router.post("/login", async (req, res, next) => {
   try {
-    res.send('Hit the users api!');
+    const { username, password } = req.body;
+
+    if(!username || !password) {
+      next({
+        name: 'Missing Credentials',
+        message: 'Please provide both username and password',
+      });
+    }
+
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password
+    const match = await bcrypt.compare(password, hashedPassword)
+    if (user && match) {
+      delete user.password;
+      const token = jwt.sign(
+        { id: user.id, username: user.username}, process.env.JWT_SECRET);
+        res.send({
+          message: "You're Logged in!",
+          token
+        });
+    } else {
+      next({
+        name: 'Incorrect Credentials',
+        message: 'Username or Password is incorrect'
+      })
+    }
   } catch (error) {
-    throw error;
+    next(error);
   }
 });
 
