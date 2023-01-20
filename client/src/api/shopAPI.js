b5a0f203be60c9eb8b45802f03f1f26db68f922c
dizzie <dizziefalls@gmail.com>
2023-01-19 22:43:25 -0600

    Fleshes out shopAPI

--- client/src/api/shopAPI.js
+++ client/src/api/shopAPI.js
@@ -4,10 +4,28 @@ const API_URL = import.meta.env.VITE_API_PATH || 'http://localhost:8080/api/'
 export const shopAPI = createApi({
   reducerPath: 'shopAPI',
   baseQuery: fetchBaseQuery({baseUrl: API_URL}),
+  tagTypes : ['Products'],
   endpoints: (builder) => ({
     getAllProducts: builder.query({
-      query: () => `products`
-    })
+      query: () => `products`,
+
+      //re-fetch *all* products upon tag invalidation
+      //we can also have individual tag subscriptions with id: id
+      providesTags: [{type: 'Products', id: 'LIST'}]
+    }),
+    //getProduct:
+    addProduct: builder.mutation({
+      query(body) {
+        return {
+          url: 'products',
+          method: 'POST',
+          body
+        }
+      },
+      invalidatesTags: [{type: 'Products', id: 'LIST'}]
+    }),
+    //updateProduct:
+    //deleteProduct:
   })
 })
 
