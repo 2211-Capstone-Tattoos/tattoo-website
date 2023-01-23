a6e61052fd780ffb0004d8040be620dca983d437
dkwhiting <dallinkwhiting@gmail.com>
2023-01-23 09:53:43 -0700

    Added single product and single artist routes

--- client/src/features/index.jsx
+++ client/src/features/index.jsx
@@ -1,13 +1,17 @@
 import Artists from './artists/Artists'
+import Artist from './artists/Artist'
 import Cart from './cart/Cart'
 import Login from './login/Login'
 import Orders from './orders/Orders'
 import Products from './products/Products'
+import Product from './products/Product'
 
 export {
   Artists,
+  Artist,
   Cart,
   Login,
   Orders,
-  Products
+  Products,
+  Product
 }
