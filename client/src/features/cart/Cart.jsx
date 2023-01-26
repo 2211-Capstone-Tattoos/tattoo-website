2c3f655e3aca3ca357527f7c26a0daaba82cf991
dizzie <dizziefalls@gmail.com>
2023-01-26 11:08:07 -0600

    Init cart in store. Reworks cart rendering and storage

--- client/src/features/cart/Cart.jsx
+++ client/src/features/cart/Cart.jsx
@@ -1,41 +1,42 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 import { useGetCartQuery } from '../../api/shopAPI';
 import './cart.css'
 
 const Cart = () => {
   const { id } = useParams();
-  const {data = [], isLoading, isFetching, isError} = useGetCartQuery(id);
-  console.log("this is data: ", data)
+  //const {data = [], isLoading, isFetching, isError} = useGetCartQuery(id);
+  const cart = useSelector((state) => state.cart.cart)
+  console.log("this is your cart: ", cart)
+
   return (
     <div>
       <h2>Your Cart</h2>
     <div className="cart">
       <div id="your-cart">
       {
-        isError
-        ? <>Ope something broke!</>
-        : isLoading || isFetching
-        ? <>Loading Cart...</>
-        : data.products.length
-        ? data.products.map(product => {
-          return (
-            <div className="product-card" key={product.id}>
-              <img src={product.img} alt="product-image" />
-              <Link to={`/products/${product.productId}`}><p>Title: {product.title}</p></Link>
-              <p>Price: {product.price}</p>
-              <p>Quantity: {product.quantity}</p>
-            </div>
-          )
-        })
-        : <>Your Cart is empty</>
+        cart
+        ? cart.products?.length
+          ? cart.products.map(product => {
+            return (
+              <div className="product-card" key={product.id}>
+                <img src={product.img} alt="product-image" />
+                <Link to={`/products/${product.productId}`}><p>Title: {product.title}</p></Link>
+                <p>Price: {product.price}</p>
+                <p>Quantity: {product.quantity}</p>
+              </div>
+            )
+          })
+          : <>Your Cart is empty</>
+        : <>Loading Cart...</>
       }
       </div>
       <div id="order-summary">
       <h3>Order Summary</h3>
         {
-        data.products
-        ? data.products.map(product => {
+        cart?.products
+        ? cart.products.map(product => {
           return (
             <div key={product.id}>
               <p>{product.price}</p>
@@ -44,7 +45,7 @@ const Cart = () => {
         }): null
         }
         <div>
-        <h4>Your Total {data?.total}</h4>
+        <h4>Your Total {cart?.total}</h4>
         <button>Purchase</button>
         </div>
       </div>
---
+++ client/src/features/cart/cartSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {} //shopAPI -> localStorage -> null
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    loadCart: (state, action) => {
+      //Init or modifying?
+      try {
+        state.cart = action.payload
+      } catch (err) {
+        console.error('Error loading cart', err)
+      }
+    },
+    addProductToCart: (state, action) => {
+      state.cart += action.payload
+    },
+    removeProductFromCart: (state, action) => {
+      // payload = productId
+      state.cart = state.cart.filter(product => product.id !== action.payload)
+    },
+    clearCart: (state) => {
+      state.cart = []
+    }
+  }
+})
+
+export const { loadCart, addToCart, removeProductFromCart, clearCart } = cartSlice.actions
+
+export default cartSlice.reducer
